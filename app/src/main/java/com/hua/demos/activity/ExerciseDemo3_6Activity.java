b765832f1b0561f9ff4704bf77b22c6e21fa399d
yejinhua <wo8013@163.com>
2017-12-15 14:52:56 +0800

    20171215     1.添加 android从入门到精通 3.6 demo(应用表格布局实现用户登录界面)

---
+++ app/src/main/java/com/hua/demos/activity/ExerciseDemo3_6Activity.java
@@ -0,0 +1,20 @@
+package com.hua.demos.activity;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.hua.R;
+
+/**
+ * Created by Administrator on 2017/12/15.
+ */
+
+public class ExerciseDemo3_6Activity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_exercise_demo3_6);
+    }
+}
--- app/src/main/java/com/hua/demos/activity/ExerciseDemoActivity.java
+++ app/src/main/java/com/hua/demos/activity/ExerciseDemoActivity.java
@@ -27,7 +27,8 @@ public class ExerciseDemoActivity extends BaseActivity implements View.OnClickLi
 
     private ExerciseBean[] exercises = new ExerciseBean[]{
             new ExerciseBean("3.2 通过代码实现游戏的进入界面",ExerciseDemo3_2Activity.class),
-            new ExerciseBean("3.4 自定义View组件实现跟随手指的兔子",ExerciseDemo3_4Activity.class)
+            new ExerciseBean("3.4 自定义View组件实现跟随手指的兔子",ExerciseDemo3_4Activity.class),
+            new ExerciseBean("3.6 用表格布局实现登录界面",ExerciseDemo3_6Activity.class)
     };
 
     @Override

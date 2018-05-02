44d420e0270fc19f723eff3ac0ecbbbe996a371f
ye <wo8013@163.com>
2018-05-02 16:09:05 +0800

    test

--- app/src/main/java/com/hua/demos/dependencies/butterknifer/ButerkniferActivity.java
+++ app/src/main/java/com/hua/demos/dependencies/butterknifer/ButerkniferActivity.java
@@ -3,7 +3,11 @@ package com.hua.demos.dependencies.butterknifer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.blankj.utilcode.utils.LogUtils;
 import com.hua.R;
+import com.hua.feature.player.utils.TimeUtils;
+
+import org.xutils.common.util.LogUtil;
 
 public class ButerkniferActivity extends AppCompatActivity {
 
@@ -11,5 +15,7 @@ public class ButerkniferActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_buterknifer);
+        LogUtil.e("s");
+        LogUtils.e("");
     }
 }
